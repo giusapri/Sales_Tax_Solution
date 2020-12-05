f1f9d4c3957e1cd0823eaa81dd471ce8401c598a
giusapri <75167599+giusapri@users.noreply.github.com>
2020-12-05 16:42:39 +0100

    Update ZeroQuantityException.java

--- src/main/java/it/giuseppeaprile/shoppingCart/exceptions/ZeroQuantityException.java
+++ src/main/java/it/giuseppeaprile/shoppingCart/exceptions/ZeroQuantityException.java
@@ -1,1 +1,9 @@
+package it.giuseppeaprile.shoppingCart.exceptions;
 
+public class ZeroQuantityException extends Exception {
+	
+	public ZeroQuantityException() {
+		super("The Item quantity in Order cannot be zero. Consider to remove object instead.");
+	}
+	
+}

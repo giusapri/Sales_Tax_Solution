6161ffe2be80d2ca8d3a3c03a735bd3d5465f4be
giusapri <75167599+giusapri@users.noreply.github.com>
2020-12-05 16:44:26 +0100

    Update TaxService.java

--- src/main/java/it/giuseppeaprile/shoppingCart/interfaces/TaxService.java
+++ src/main/java/it/giuseppeaprile/shoppingCart/interfaces/TaxService.java
@@ -1,1 +1,13 @@
+package it.giuseppeaprile.shoppingCart.interfaces;
+
+import java.math.BigDecimal;
+
+import it.giuseppeaprile.shoppingCart.entities.Item;
+
+public interface TaxService {
+	
+	public abstract BigDecimal calculateImportTaxes(Item item);
+	public abstract BigDecimal calculateBasicTaxes(Item item);
+	
+}
 

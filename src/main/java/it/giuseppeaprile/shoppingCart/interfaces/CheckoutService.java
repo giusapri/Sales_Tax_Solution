7c9f9c67a40c06eacfab5cf199e67c238cb0c36e
giusapri <75167599+giusapri@users.noreply.github.com>
2020-12-05 16:44:03 +0100

    Update CheckoutService.java

--- src/main/java/it/giuseppeaprile/shoppingCart/interfaces/CheckoutService.java
+++ src/main/java/it/giuseppeaprile/shoppingCart/interfaces/CheckoutService.java
@@ -1,1 +1,9 @@
+package it.giuseppeaprile.shoppingCart.interfaces;
+
+import it.giuseppeaprile.shoppingCart.entities.ShoppingCart;
+
+public interface CheckoutService {
+	public abstract void calculateTotal(ShoppingCart shoppingCart);
+	public abstract String printReceipt(ShoppingCart shoppingCart);
+}
 

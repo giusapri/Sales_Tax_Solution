9ff052b37373bd0b111c9b09d1178517ba922ffd
giusapri <75167599+giusapri@users.noreply.github.com>
2020-12-05 16:42:09 +0100

    Update NoShoppingCartFoundException.java

--- src/main/java/it/giuseppeaprile/shoppingCart/exceptions/NoShoppingCartFoundException.java
+++ src/main/java/it/giuseppeaprile/shoppingCart/exceptions/NoShoppingCartFoundException.java
@@ -1,1 +1,10 @@
+package it.giuseppeaprile.shoppingCart.exceptions;
+
+public class NoShoppingCartFoundException extends Exception {
+
+	public NoShoppingCartFoundException() {
+		super("No Shopping Cart object found. Consider to set a shopping cart before doing operations");
+	}
+	
+}
 

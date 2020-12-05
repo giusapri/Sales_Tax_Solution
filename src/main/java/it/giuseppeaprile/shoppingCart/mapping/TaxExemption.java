d23ceb360b95904707ee3179de08e0ad7ab9ecf2
giusapri <75167599+giusapri@users.noreply.github.com>
2020-12-05 16:46:34 +0100

    Update TaxExemption.java

--- src/main/java/it/giuseppeaprile/shoppingCart/mapping/TaxExemption.java
+++ src/main/java/it/giuseppeaprile/shoppingCart/mapping/TaxExemption.java
@@ -1,1 +1,26 @@
+package it.giuseppeaprile.shoppingCart.mapping;
+
+import java.util.HashSet;
+import it.giuseppeaprile.shoppingCart.enums.ItemType;
+
+public class TaxExemption {
+
+	public HashSet<ItemType> basicSalesExemption;
+	
+	public TaxExemption() {
+		basicSalesExemption = new HashSet<>();
+		basicSalesExemption.add(ItemType.BOOK);
+		basicSalesExemption.add(ItemType.FOOD);
+		basicSalesExemption.add(ItemType.MEDICAL);
+	}
+	
+	public HashSet<ItemType> getBasicSalesExemption() {
+		return basicSalesExemption;
+	}
+	
+	public boolean isItemExempt(ItemType type) {
+		return basicSalesExemption.contains(type);
+	}
+	
+}
 

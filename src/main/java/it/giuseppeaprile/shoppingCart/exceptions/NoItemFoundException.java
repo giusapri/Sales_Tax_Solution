5fd713133cb340689089c2fe55cd3c596d058352
giusapri <75167599+giusapri@users.noreply.github.com>
2020-12-05 16:41:33 +0100

    Update NoItemFoundException.java

--- src/main/java/it/giuseppeaprile/shoppingCart/exceptions/NoItemFoundException.java
+++ src/main/java/it/giuseppeaprile/shoppingCart/exceptions/NoItemFoundException.java
@@ -1,1 +1,10 @@
+package it.giuseppeaprile.shoppingCart.exceptions;
+
+public class NoItemFoundException extends Exception {
+
+	public NoItemFoundException() {
+		super("No Item object found");
+	}
+	
+}
 
